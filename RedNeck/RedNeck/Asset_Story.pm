567a3fca03ef4096a52dd88b8ef0c6a596edaca3
webgui <webgui@frozen.plainblack.com>
2011-04-28 17:17:31 -0500

    Updating RedNeck on translation server

--- RedNeck/RedNeck/Asset_Story.pm
+++ RedNeck/RedNeck/Asset_Story.pm
@@ -1,5 +1,11 @@
 package WebGUI::i18n::RedNeck::Asset_Story;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'Source' => {
+    'lastUpdated' => '1304029041',
+    'message' => 'Where did ya get it?'
+  }
+}
+;
 
 1;
