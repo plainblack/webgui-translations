8cccf40ca9ad36c45ffbe8ebd626eee0a4e3f14e
webgui <webgui@frozen.plainblack.com>
2011-05-05 09:48:55 -0500

    Updating Dutch on translation server

--- Dutch/Dutch/Asset_Dashboard.pm
+++ Dutch/Dutch/Asset_Dashboard.pm
@@ -29,6 +29,10 @@ our $I18N = {
     'lastUpdated' => '1304599110',
     'message' => 'Kan het dashlet verplaatst worden op het dashboard door gebruikers?'
   },
+  'adminsGroupId' => {
+    'lastUpdated' => '1304605941',
+    'message' => 'De GUID van de groep die het standaarscherm van het dahsvoard voor bezoekers mag instellen.'
+  },
   'assetName' => {
     'lastUpdated' => '1152624242',
     'message' => 'Dashboard'
@@ -41,6 +45,30 @@ our $I18N = {
     'lastUpdated' => '1152623534',
     'message' => 'Deze lijst bevat een vak voor elk type asset van de pagina layout dat aangevinkt kan worden. Vink dit vak aan bij elke asset die niet getoond moet worden in de pagina layout asset. '
   },
+  'canDelete' => {
+    'lastUpdated' => '1304606149',
+    'message' => 'Een functie die waar is als de huidige gebruiker in de groep zit die het dashboard naar eigen smaak kan aanpassen en het dashlet niet op vereist is ingesteld.'
+  },
+  'canEdit' => {
+    'lastUpdated' => '1304606055',
+    'message' => 'Een functie die waar is als de huidige gebruiker in de groep zit die het dashboard naar eigen smaak kan aanpassen.'
+  },
+  'canEditUserPrefs' => {
+    'lastUpdated' => '1304606125',
+    'message' => 'Een functie die waar is als de huidige gebruiker in de geregistreerde gebruikers groep zit, het dashlet een snelkoppeling is en de snelkoppeling configureerbare instellingen heeft.'
+  },
+  'canMove' => {
+    'lastUpdated' => '1304606139',
+    'message' => 'Een functie die waar is als de huidige gebruiker in de groep zit die het dashboard naar eigen smaak kan aanpassen en het dashlet niet op statisch is ingesteld.'
+  },
+  'canPersonalize' => {
+    'lastUpdated' => '1304606164',
+    'message' => 'Een functie die waar is als de huidige gebruiker in de groep zit die het dashboard naar eigen smaak kan aanpassen.'
+  },
+  'content' => {
+    'lastUpdated' => '1304606181',
+    'message' => 'De inhoud van het dashlet'
+  },
   'dashboard adminsGroupId description' => {
     'lastUpdated' => '1152861173',
     'message' => 'Welke groep mag het dashboard wijzigen? Dit houdt in het wijzigen/verwijderen/toevoegen van beschikbare dashlets, instellingen en sjablonen '
@@ -48,6 +76,10 @@ our $I18N = {
   'dashboard adminsGroupId field label' => {
     'lastUpdated' => '1152861653',
     'message' => 'Wie kan beheren?'
+  },
+  'dashboard asset template variables title' => {
+    'lastUpdated' => '1304606219',
+    'message' => 'Dashboard sjabloonvariabelen'
   },
   'dashboard template description' => {
     'lastUpdated' => '1152861641',
@@ -65,6 +97,26 @@ our $I18N = {
     'lastUpdated' => '1152862184',
     'message' => 'Wie kan personaliseren?'
   },
+  'dashletTitle' => {
+    'lastUpdated' => '1304606243',
+    'message' => 'De titel van het dashlet.'
+  },
+  'dashletUrl' => {
+    'lastUpdated' => '1304606255',
+    'message' => 'De URL van het dashlet.'
+  },
+  'dragger.init' => {
+    'lastUpdated' => '1304606303',
+    'message' => 'Javascript is nodig van het initialiseren van het dashboard. Het dient aan de onderkant van het sjabloon geplaatst te worden.'
+  },
+  'editFormUrl' => {
+    'lastUpdated' => '1304606403',
+    'message' => 'De URL waar de het formulier voor de gebruikersinstellingen van het dashlet opgehaald kunnen worden, om in te stellen of het een snelkoppeling of een gewoon asset is.'
+  },
+  'fullUrl' => {
+    'lastUpdated' => '1304605975',
+    'message' => 'Het volledige URL van het dashboard, met sitename en gateway configuratie.'
+  },
   'hide new content list' => {
     'lastUpdated' => '1231700981',
     'message' => 'Lijst met verborgen nieuwe inhoud'
@@ -72,6 +124,14 @@ our $I18N = {
   'id' => {
     'lastUpdated' => '1304599142',
     'message' => 'Asset ID van het huidige dashlet.'
+  },
+  'isInitialized' => {
+    'lastUpdated' => '1304606777',
+    'message' => 'Een functie die waar is als het dashboard geinitialiseerd is.'
+  },
+  'positionN_loop' => {
+    'lastUpdated' => '1304606901',
+    'message' => 'Standaard zijn er 4 posities,genummerd 1 tm 4. Elke functie bevat een lijst met assets die erin geplaatst zijn. De eerste positie is speciaal, omdat het ook de assets bevat die niet specifiek geplaatst zijn door de gebruiker.'
   },
   'shortcutUrl' => {
     'lastUpdated' => '1304599307',
