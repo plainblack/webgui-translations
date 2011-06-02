e5100403dd29516b3534197fe29fce7153bf0cd7
webgui <webgui@frozen.plainblack.com>
2011-06-02 12:04:25 -0500

    Updating German on translation server

--- German/German/Macro_RenderThingData.pm
+++ German/German/Macro_RenderThingData.pm
@@ -1,6 +1,10 @@
 package WebGUI::i18n::German::Macro_RenderThingData;
 use utf8;
 our $I18N = {
+  'bad tags' => {
+    'lastUpdated' => '1306428739',
+    'message' => ''
+  },
   'no template' => {
     'lastUpdated' => '1306299924',
     'message' => 'RenderThingData: Bitte geben Sie ein Template an.'
