e568c380a0b83dc835ea325f5ce283979877bb48
webgui <webgui@frozen.plainblack.com>
2011-07-26 02:24:17 -0500

    Updating German on translation server

--- German/German/Activity_WaitForUserConfirmation.pm
+++ German/German/Activity_WaitForUserConfirmation.pm
@@ -1,5 +1,71 @@
 package WebGUI::i18n::German::Activity_WaitForUserConfirmation;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'doOnExpire hoverHelp' => {
+    'lastUpdated' => '1311664588',
+    'message' => 'Workflow der läuft, wenn die Wartezeit überschritten ist.'
+  },
+  'doOnExpire label' => {
+    'lastUpdated' => '1311664611',
+    'message' => 'Wenn abgelaufen'
+  },
+  'emailFrom hoverHelp' => {
+    'lastUpdated' => '1311664644',
+    'message' => 'Von welchem Benutzer soll die Bestätigungs-E-Mail kommen?'
+  },
+  'emailFrom label' => {
+    'lastUpdated' => '1311664655',
+    'message' => 'E-Mail "Von"'
+  },
+  'emailSubject label' => {
+    'lastUpdated' => '1311664670',
+    'message' => 'E-Mail "Betreff"'
+  },
+  'expireAfter hoverHelp' => {
+    'lastUpdated' => '1311664694',
+    'message' => 'Wie lange soll auf eine Benutzer-Antwort gewartet werden?'
+  },
+  'expireAfter label' => {
+    'lastUpdated' => '1311664717',
+    'message' => 'Verfalle nach'
+  },
+  'okMessage hoverHelp' => {
+    'lastUpdated' => '1311664759',
+    'message' => 'Nachricht die dem Benutzer angezeigt wird, wenn er den Bestätigungs-Link anklickt.'
+  },
+  'okMessage label' => {
+    'lastUpdated' => '1311664826',
+    'message' => 'Bestätigungs-Meldung'
+  },
+  'template hoverHelp' => {
+    'lastUpdated' => '1311664855',
+    'message' => 'Template-Code für den E-Mail-Body'
+  },
+  'template label' => {
+    'lastUpdated' => '1311664860',
+    'message' => 'Template'
+  },
+  'templateParser label' => {
+    'lastUpdated' => '1311664872',
+    'message' => 'Template-Parser'
+  },
+  'topicName' => {
+    'lastUpdated' => '1311664889',
+    'message' => 'Warte auf Benutzer-Bestätigung'
+  },
+  'waitBetween hoverHelp' => {
+    'lastUpdated' => '1311664939',
+    'message' => 'Wie lange soll zwischen den Prüfungen, ob der Benutzer den Link angeklickt hat, gewaret werden?'
+  },
+  'waitBetween label' => {
+    'lastUpdated' => '1311665036',
+    'message' => 'Warte-Intervall'
+  },
+  'your template goes here' => {
+    'lastUpdated' => '1311665052',
+    'message' => 'Hier das Template'
+  }
+}
+;
 
 1;
