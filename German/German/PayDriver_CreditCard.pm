46dee720b259bec79fd811d57849369ca7df5cab
webgui <webgui@frozen.plainblack.com>
2011-08-31 23:41:44 -0500

    Updating German on translation server

--- German/German/PayDriver_CreditCard.pm
+++ German/German/PayDriver_CreditCard.pm
@@ -1,5 +1,55 @@
 package WebGUI::i18n::German::PayDriver_CreditCard;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'cardNumber' => {
+    'lastUpdated' => '1314851541',
+    'message' => 'Kreditkarte-Nummer'
+  },
+  'cardNumberField help' => {
+    'lastUpdated' => '1314851585',
+    'message' => 'Textfeld zur Eingabe der Kreditkarte-Nummer'
+  },
+  'checkoutButton help' => {
+    'lastUpdated' => '1314851739',
+    'message' => 'Bezeichneter Knopf zum Abschicken des Formulars und Weiterführung des laufenden Prozesses'
+  },
+  'credentials template' => {
+    'lastUpdated' => '1314852075',
+    'message' => 'Vorlage Kreditkartenangaben'
+  },
+  'cvv2' => {
+    'lastUpdated' => '1314851777',
+    'message' => 'Verifizierungsnummer (CVV2)'
+  },
+  'expiration date' => {
+    'lastUpdated' => '1314851999',
+    'message' => 'Gültig bis'
+  },
+  'invalid address' => {
+    'lastUpdated' => '1314851894',
+    'message' => 'Die Adressangabe ist ungültig'
+  },
+  'invalid city' => {
+    'lastUpdated' => '1314851874',
+    'message' => 'Der Ort ist ungültig'
+  },
+  'invalid email' => {
+    'lastUpdated' => '1314851856',
+    'message' => 'Die Email-Adresse ist ungültig'
+  },
+  'invalid firstName' => {
+    'lastUpdated' => '1314851928',
+    'message' => 'Der Vorname ist ungültig'
+  },
+  'invalid zip' => {
+    'lastUpdated' => '1314851953',
+    'message' => 'Die Postleitzahl ist ungültig'
+  },
+  'use cvv2' => {
+    'lastUpdated' => '1314851801',
+    'message' => 'CVV2 benutzen'
+  }
+}
+;
 
 1;
