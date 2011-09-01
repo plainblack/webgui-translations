083e3fc4c255298b752d8a42bb236dcacdeab7eb
webgui <webgui@frozen.plainblack.com>
2011-09-01 02:14:43 -0500

    Updating German on translation server

--- German/German/PayDriver_CreditCard.pm
+++ German/German/PayDriver_CreditCard.pm
@@ -1,6 +1,10 @@
 package WebGUI::i18n::German::PayDriver_CreditCard;
 use utf8;
 our $I18N = {
+  'addressField help' => {
+    'lastUpdated' => '1314860964',
+    'message' => 'Ein einzelnes Textfeld für die Eingabe der Wohnadresse'
+  },
   'cardNumber' => {
     'lastUpdated' => '1314851541',
     'message' => 'Kreditkarte-Nummer'
@@ -21,33 +25,65 @@ our $I18N = {
     'lastUpdated' => '1314851777',
     'message' => 'Verifizierungsnummer (CVV2)'
   },
+  'emailField help' => {
+    'lastUpdated' => '1314861247',
+    'message' => 'Einzelnes Textfeld für die Eingabe der E-Mail-Adresse'
+  },
   'expiration date' => {
     'lastUpdated' => '1314851999',
     'message' => 'Gültig bis'
+  },
+  'expired expiration date' => {
+    'lastUpdated' => '1314861223',
+    'message' => 'Gültigkeitsdatum abgelaufen'
   },
   'invalid address' => {
     'lastUpdated' => '1314851894',
     'message' => 'Die Adressangabe ist ungültig'
   },
+  'invalid card number' => {
+    'lastUpdated' => '1314860759',
+    'message' => 'Die Kreditkartennummer ist ungültig'
+  },
   'invalid city' => {
     'lastUpdated' => '1314851874',
     'message' => 'Der Ort ist ungültig'
+  },
+  'invalid cvv2' => {
+    'lastUpdated' => '1314861139',
+    'message' => 'Die Kreditkartenprüfnummer ist ungültig'
   },
   'invalid email' => {
     'lastUpdated' => '1314860665',
     'message' => 'Die E-Mail-Adresse ist ungültig'
   },
+  'invalid expiration date' => {
+    'lastUpdated' => '1314860821',
+    'message' => 'Das Gültigkeitsdatum ist ungültig'
+  },
   'invalid firstName' => {
     'lastUpdated' => '1314851928',
     'message' => 'Der Vorname ist ungültig'
+  },
+  'invalid lastName' => {
+    'lastUpdated' => '1314860845',
+    'message' => 'Der Nachname ist ungültig'
   },
   'invalid zip' => {
     'lastUpdated' => '1314851953',
     'message' => 'Die Postleitzahl ist ungültig'
   },
+  'monthYearField help' => {
+    'lastUpdated' => '1314860922',
+    'message' => 'Ein kombiniertes Formularfeld, um Monat und Jahr des Gültigkeitsdatums der Kreditkarte einzugeben.'
+  },
   'use cvv2' => {
     'lastUpdated' => '1314851801',
     'message' => 'CVV2 benutzen'
+  },
+  'use cvv2 help' => {
+    'lastUpdated' => '1314861187',
+    'message' => 'Setzen der Option für die Nutzung der Kteditkartenprüfnummer CVV2 (Kartenrückseite)'
   }
 }
 ;
