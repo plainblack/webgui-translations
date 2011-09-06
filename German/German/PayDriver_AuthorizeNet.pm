de9a34ea5fc7022886db63359a82ca427003f42e
webgui <webgui@frozen.plainblack.com>
2011-09-06 02:20:28 -0500

    Updating German on translation server

--- German/German/PayDriver_AuthorizeNet.pm
+++ German/German/PayDriver_AuthorizeNet.pm
@@ -1,5 +1,40 @@
 package WebGUI::i18n::German::PayDriver_AuthorizeNet;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'cardType' => {
+    'lastUpdated' => '1315293384',
+    'message' => 'Kartentyp'
+  },
+  'login' => {
+    'lastUpdated' => '1315293400',
+    'message' => 'API Login'
+  },
+  'login help' => {
+    'lastUpdated' => '1315293434',
+    'message' => 'Die API Login ID für Ihr AuthorizeNet Konto'
+  },
+  'name' => {
+    'lastUpdated' => '1315293459',
+    'message' => 'Kreditkarte (Authorize.net)'
+  },
+  'test mode' => {
+    'lastUpdated' => '1315293469',
+    'message' => 'Testmodus'
+  },
+  'test mode help' => {
+    'lastUpdated' => '1315293546',
+    'message' => 'Legt fest, ob die Verwendung von AuthorizeNet in Testmodus abläuft.'
+  },
+  'transaction key' => {
+    'lastUpdated' => '1315293572',
+    'message' => 'Transaction Key'
+  },
+  'transaction key help' => {
+    'lastUpdated' => '1315293619',
+    'message' => 'Der Transaction Key für Ihr Authorize.net Konto
+'
+  }
+}
+;
 
 1;
