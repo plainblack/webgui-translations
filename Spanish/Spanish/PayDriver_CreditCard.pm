65b5735823070e3da15139ece3fe369fd84a510b
webgui <webgui@frozen.plainblack.com>
2011-10-26 21:41:51 -0500

    Updating Spanish on translation server

--- Spanish/Spanish/PayDriver_CreditCard.pm
+++ Spanish/Spanish/PayDriver_CreditCard.pm
@@ -1,5 +1,127 @@
 package WebGUI::i18n::Spanish::PayDriver_CreditCard;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'addressField help' => {
+    'lastUpdated' => '1319682704',
+    'message' => 'Un campo de texto para que el usuario ingrese su dirección.'
+  },
+  'cardNumber' => {
+    'lastUpdated' => '1319682682',
+    'message' => 'Número de Tarjeta'
+  },
+  'cardNumberField help' => {
+    'lastUpdated' => '1319682725',
+    'message' => 'Un campo de texto para que el usuario ingrese el número de tarjeta de crédito.'
+  },
+  'checkoutButton help' => {
+    'lastUpdated' => '1319682768',
+    'message' => 'Un botón con el mensaje localizado que permite enviar el formulario y continuar el proceso de pago.'
+  },
+  'credentials template' => {
+    'lastUpdated' => '1319682778',
+    'message' => 'Plantilla para Credenciales'
+  },
+  'credentials template help' => {
+    'lastUpdated' => '1319682811',
+    'message' => 'Indique la Plantilla a emplear para que el usuario suministre la información de cobranza y tarjeta de crédito.'
+  },
+  'cvv2' => {
+    'lastUpdated' => '1319682832',
+    'message' => 'Dígitos verificadores (CVV2)'
+  },
+  'cvv2Field help' => {
+    'lastUpdated' => '1319682866',
+    'message' => 'Un campo de texto para que el usuario ingrese los dígitos verificadores de la tarjeta de crédito. Si el método de pago no ha sido configurado para usar CVV2, el campo estará vacío.'
+  },
+  'edit credentials template' => {
+    'lastUpdated' => '1319682876',
+    'message' => 'Plantilla para Editar Credenciales'
+  },
+  'edit credentials template help' => {
+    'lastUpdated' => '1319682913',
+    'message' => 'Indique la Plantilla a emplear para presentar un formulario que el usuario usará para incluir información de contacto y tarjeta de crédito para cobranza.'
+  },
+  'emailField help' => {
+    'lastUpdated' => '1319682930',
+    'message' => 'Un campo de texto para que el usuario ingrese su dirección de correo elecrónico.'
+  },
+  'error help' => {
+    'lastUpdated' => '1319682951',
+    'message' => 'Un error del bucle de errores, con el mínimo markup.'
+  },
+  'error occurred message' => {
+    'lastUpdated' => '1319682963',
+    'message' => 'Han ocurrido los siguientes errores'
+  },
+  'errors help' => {
+    'lastUpdated' => '1319682985',
+    'message' => 'Un bucle que itera sobre todos los errores encontrados al procesar el formulario.'
+  },
+  'expiration date' => {
+    'lastUpdated' => '1319682994',
+    'message' => 'Expira en'
+  },
+  'expired expiration date' => {
+    'lastUpdated' => '1319683019',
+    'message' => 'Su tarjeta de crédito ha expirado.'
+  },
+  'fields help' => {
+    'lastUpdated' => '1319683089',
+    'message' => 'Un bucle que itera sobre todos los campos disponibles. Cada pasada por el bucle contiene el nombre (field name), etiqueta (con el texto localizado) y campo.'
+  },
+  'invalid address' => {
+    'lastUpdated' => '1319683107',
+    'message' => 'Ha suministrado una dirección inválida.'
+  },
+  'invalid card number' => {
+    'lastUpdated' => '1319683120',
+    'message' => 'Ha suministrado un número de tarjeta de crédito inválido.'
+  },
+  'invalid city' => {
+    'lastUpdated' => '1319683131',
+    'message' => 'Ha suministrado una ciudad inválida.'
+  },
+  'invalid cvv2' => {
+    'lastUpdated' => '1319683157',
+    'message' => 'Ha suministrado dígitos de verificación (CVV2) inválidos.'
+  },
+  'invalid email' => {
+    'lastUpdated' => '1319683169',
+    'message' => 'Ha suministrado una dirección de correo electrónico inválida.'
+  },
+  'invalid expiration date' => {
+    'lastUpdated' => '1319683182',
+    'message' => 'Ha suministrado una fecha de expiración inválida.'
+  },
+  'invalid firstName' => {
+    'lastUpdated' => '1319683193',
+    'message' => 'Ha suministrado un nombre inválido.'
+  },
+  'invalid lastName' => {
+    'lastUpdated' => '1319683202',
+    'message' => 'Ha suministrado un apellido inválido.'
+  },
+  'invalid zip' => {
+    'lastUpdated' => '1319683211',
+    'message' => 'Ha suministrado una zona postal inválida.'
+  },
+  'monthYearField help' => {
+    'lastUpdated' => '1319683238',
+    'message' => 'Un campo formulario combinado para que el usuario ingrese el mes y el año de expiración de su tarjeta de crédito.'
+  },
+  'template gone' => {
+    'lastUpdated' => '1319683260',
+    'message' => 'La Plantilla para suministrar las credenciales ha sido eliminada. Por favor notifique al administrador del sistema.'
+  },
+  'use cvv2' => {
+    'lastUpdated' => '1319683269',
+    'message' => 'Usar CVV2'
+  },
+  'use cvv2 help' => {
+    'lastUpdated' => '1319683285',
+    'message' => 'Indica si se desea utilizar los dígitos de validación CVV2 o no.'
+  }
+}
+;
 
 1;
